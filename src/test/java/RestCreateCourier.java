efc534f63dd0cea3d4333de0884ac92d60212f6a
Sergey Vasilyev <s.s.vasilev@gmail.com>
2023-01-19 01:41:24 +0300

    Сделал запрос на создание и логин курьера

---
+++ src/test/java/RestCreateCourier.java
@@ -0,0 +1,66 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+import io.qameta.allure.junit4.DisplayName; // импорт DisplayName
+import io.qameta.allure.Description; // импорт Description
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+public class RestCreateCourier {
+
+    String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzc3NzQwMjJkMTM2ZDAwM2RkODBkNTciLCJpYXQiOjE2NzM3ODE3NDcsImV4cCI6MTY3NDM4NjU0N30.ODkAuVhkGSLxdtW5_NiBvXbJiDLDUseIHp7rfZ-6XgA";
+
+    @Before
+    public void setUp() {
+
+        RestAssured.baseURI= "http://qa-scooter.praktikum-services.ru";
+    }
+
+    @Test
+    public void getCourierId() {
+        String json = "{\"login\": \"AutoTestCourier\", \"password\": \"1234\"}";
+        CourierId courierId =
+                 given()
+                 .header("Content-type", "application/json")
+                 .body(json)
+                 .post("/api/v1/courier/login")
+                 .as(CourierId.class);
+
+        System.out.println(courierId.id);
+
+
+    }
+
+
+    @Test
+    @DisplayName("Создание курьера (курьера можно создать)") // имя теста
+    @Description("Позитивный") // описание теста
+    public void getMyInfoStatusCode() {
+        String json = "{\"login\": \"AutoTestCourier\", \"password\": \"1234\", \"firstName\": \"Иван\"}";
+        Response response =
+                given()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/courier");
+        response.then().assertThat().body("ok", CoreMatchers.equalTo("true"))
+                .and()
+                .statusCode(201);
+    }
+
+
+    public class CourierId {
+        private int id;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+    }
+}
